a39b085ec4a0460d91dcb7bb4d027a8b50c239cf
Miley Wright <dishaliekwright@yahoo.com>
2020-04-01 00:00:33 -0400

    POST user route

--- exercise-tracker/backend/routes/users.js
+++ exercise-tracker/backend/routes/users.js
@@ -3,12 +3,24 @@ const User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     User.find()
-    .then(user => 
+    .then(user =>{ 
         res.status(200).json(user)
-    )
+    })
     .catch(err => 
         res.status(400).json('Error: ', err)
     )
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const newUser = new User({username});
+    User.save()
+    .then(added => {
+        res.status(201).json(added)
+    })
+    .catch(err => {
+        res.status(400).json('Error: ', err)
+    })
 });
 
 module.exports = router;
