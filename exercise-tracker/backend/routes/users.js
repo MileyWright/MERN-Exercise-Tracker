2cb496771b4bc475d5dfb16a30c04e75088fdcb9
Miley Wright <dishaliekwright@yahoo.com>
2020-03-31 23:43:19 -0400

    GET user route

---
+++ exercise-tracker/backend/routes/users.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+    User.find()
+    .then(user => 
+        res.status(200).json(user)
+    )
+    .catch(err => 
+        res.status(400).json('Error: ', err)
+    )
+});
+
+module.exports = router;
